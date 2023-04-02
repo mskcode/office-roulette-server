03ece7bab7855d0ab034887896d433e2bcdf0637
Samuli Kolehmainen <samuli.kolehmainen@veikkaus.fi>
2023-04-02 11:48:06 +0300

    CreateNewEmployeeRequestDto: define JsonProperty name mappings

--- src/main/java/fi/mskcode/officeroulette/api/CreateNewEmployeeRequestDto.java
+++ src/main/java/fi/mskcode/officeroulette/api/CreateNewEmployeeRequestDto.java
@@ -3,12 +3,16 @@ package fi.mskcode.officeroulette.api;
 import static fi.mskcode.officeroulette.util.DateTimeUtil.parseIso8601String;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.Instant;
 
 record CreateNewEmployeeRequestDto(String firstName, String lastName, Instant employmentStartTime) {
 
     @JsonCreator
-    public CreateNewEmployeeRequestDto(String firstName, String lastName, String employmentStartTime) {
+    public CreateNewEmployeeRequestDto(
+            @JsonProperty("firstName") String firstName,
+            @JsonProperty("lastName") String lastName,
+            @JsonProperty("employmentStartTime") String employmentStartTime) {
         this(firstName, lastName, parseIso8601String(employmentStartTime));
     }
 }
