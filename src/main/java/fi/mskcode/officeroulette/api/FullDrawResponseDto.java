ea38f2c468b44ad4b6859994e97746085be715c8
Samuli Kolehmainen <samuli.kolehmainen@veikkaus.fi>
2023-04-03 08:04:57 +0300

    FullDrawResponseDto: Add insertTime and drawTime fields

--- src/main/java/fi/mskcode/officeroulette/api/FullDrawResponseDto.java
+++ src/main/java/fi/mskcode/officeroulette/api/FullDrawResponseDto.java
@@ -1,17 +1,27 @@
 package fi.mskcode.officeroulette.api;
 
 import static com.google.common.collect.ImmutableList.toImmutableList;
+import static fi.mskcode.officeroulette.util.DateTimeUtil.formatAsIso8601;
 
 import fi.mskcode.officeroulette.core.FullDraw;
+import fi.mskcode.officeroulette.util.DateTimeUtil;
 import java.util.List;
+import java.util.Optional;
 
 public record FullDrawResponseDto(
-        long id, String status, List<EmployeeResponseDto> participants, DrawResultResponseDto result) {
+        long id,
+        String status,
+        String insertTime,
+        Optional<String> drawTime,
+        List<EmployeeResponseDto> participants,
+        DrawResultResponseDto result) {
 
     public static FullDrawResponseDto from(FullDraw draw) {
         return new FullDrawResponseDto(
                 draw.draw().id(),
                 draw.draw().status().name(),
+                formatAsIso8601(draw.draw().insertTime()),
+                draw.draw().drawTime().map(DateTimeUtil::formatAsIso8601),
                 draw.participants().stream().map(EmployeeResponseDto::from).collect(toImmutableList()),
                 draw.result().map(DrawResultResponseDto::from).orElse(null));
     }
